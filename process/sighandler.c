b0a51c8ca6f83ce0beccded43b7f95683755b1cd
EngineersBox <toastcraft.info@gmail.com>
2023-08-08 20:45:03 +1000

    Implemented signal handler

--- process/sighandler.c
+++ process/sighandler.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <signal.h>
 
-// TODO: write a function to handle signal
-void handler(int sig)
-{
-    // TODO
+void handler(int sig) {
+    printf("Signal %d received\n", sig);
 }
 
-int main()
-{
-    // TODO: register your signal handler here
-
-    return 0; 
+int main() {
+	struct sigaction handler_action = { 0 };
+	handler_action.sa_handler = &handler;
+	if (sigaction(SIGINT, &handler_action, NULL) < 0) {
+		perror("sighandler: sigaction failed");
+		return 1;
+	}
+	printf("Hello, send me a signal. My proces id is %d\n", getpid());
+	printf("Type 'exit' to quit this program at any time.\n");
+	char input[4];
+	while (1) {
+		printf("(waiting for signal) >");
+		scanf("%s", input);
+		if (strncmp(input, "exit", 4) == 0) {
+			return 0;
+		}
+	}
+	return 0; 
 }
 
